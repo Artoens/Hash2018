b8b2010d68afb39c8a49269bbc574ffb4458b413
Guillaume <guillaumedemoff@gmail.com>
2018-03-01 20:17:02 +0100

    Add import from file

--- Hash2018/Hash2018/Program.cs
+++ Hash2018/Hash2018/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,28 @@ namespace Hash2018
     {
         static void Main(string[] args)
         {
+            int[][] input = ReadFile("C:/Users/guill/Desktop/Hash2018/a_example.in");
+           
+            /*
+            foreach(int[] a in input)
+            {
+                foreach(int b in a)
+                {
+                    Console.Write(b + " ");
+                }
+                Console.Write('\n');
+            }
+            Console.ReadKey();*/
+        }
+        private static int[][] ReadFile(string path)
+        {
+
+            //list of lines, blank lines are removed 
+            string[] lines = File.ReadAllLines(path).Where(arg =>
+                                !string.IsNullOrWhiteSpace(arg)).ToArray();
+
+            // Split every lines and Trim  
+            return lines.Select(x => x.Split(' ').Select(y => Convert.ToInt32(y.Trim())).ToArray()).ToArray();
         }
     }
 }
