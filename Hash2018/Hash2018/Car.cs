b3140c222a2e9e0631c2aa25a0ff8c17d7984ecd
Martin DEGELDT <15076@ecam.be>
2018-03-01 21:15:06 +0100

    Car + ride

--- Hash2018/Hash2018/Car.cs
+++ Hash2018/Hash2018/Car.cs
@@ -8,19 +8,31 @@ namespace Hash2018
 {
     class Car
     {
-        private int posX;
-        private int posY;
+        private int pos = 0;
         private List<Ride> rides;
 
-        public Car(int posX, int posY, List<Ride> rides)
+        public Car(int posY, List<Ride> rides)
         {
-            this.posX = posX;
-            this.posY = posY;
             this.rides = rides;
         }
 
-        public int PosX => posX;
-        public int PosY => posY;
+
+        public int Pos => pos;
         public List<Ride> Rides => rides;
+
+        public void AddRide(Ride r)
+        {
+            if (rides.Count() != 0)
+            {
+                pos += r.Delay;
+            }
+            else
+            {
+                pos += Math.Abs(r.Delay - r.Start.X - r.Start.Y);
+            }
+            pos += r.Poid();
+            rides.Add(r);
+        }
+
     }
 }
